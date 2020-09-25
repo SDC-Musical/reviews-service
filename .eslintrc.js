2ada39063408d88057966cdf52578962b29d476c
Tandy Nguyen <nguyetan001@gmail.com>
2020-09-24 23:33:31 -0400

    Corrected ESLint config to match teams

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,22 @@
 module.exports = {
-  extends: 'airbnb',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
   rules: {
     'no-console': 'off',
   },
