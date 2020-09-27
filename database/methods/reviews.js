f8111c8e08bfae2cab7ccd4d5b863b264f779ace
Tandy Nguyen <nguyetan001@gmail.com>
2020-09-27 18:45:50 -0400

    review_id now increments upon added review

--- database/methods/reviews.js
+++ database/methods/reviews.js
@@ -1,6 +1,14 @@
 const ReviewModel = require('../models/reviews.js');
+const CounterModel = require('../models/counter.js');
 
-module.exports.addReview = (review) => {
+module.exports.addReview = async (review) => {
+  const counter = await CounterModel.findOneAndUpdate(
+    { model_name: 'review' },
+    { $inc: { seq: 1 } },
+    { upsert: true },
+  );
+  // eslint-disable-next-line no-param-reassign
+  review.review_id = (counter) ? counter.seq + 1 : 1;
   const reviewDoc = new ReviewModel(review);
   return reviewDoc.save();
 };
--- database/models/reviews.js
+++ database/models/reviews.js
@@ -1,24 +1,12 @@
 const mongoose = require('mongoose');
-const CounterModel = require('./counter.js');
 
 const reviewSchema = new mongoose.Schema({
-  review_id: { type: Number, default: 0 },
+  review_id: { type: Number, unique: true },
   product_id: { type: Number, required: true },
   username: { type: String, required: true },
   review_text: String,
   review_rating: { type: Number, required: true },
   created_at: { type: Date, default: Date.now },
-});
-
-reviewSchema.pre('save', async () => {
-  await CounterModel.findOneAndUpdate(
-    { model_name: 'review' },
-    { $inc: { seq: 1 } },
-    { upsert: true },
-  );
-});
-
-reviewSchema.post('save', (doc) => {
 });
 
 module.exports = mongoose.model('Review', reviewSchema);
