dd9c5717d9ae046a65a323650efa08001f97e4c0
Tandy Nguyen <nguyetan001@gmail.com>
2020-09-27 16:43:22 -0400

    Created counter model

---
+++ database/models/reviews.test.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+// const ReviewModel = require('./reviews.js');
+
+describe('Review Model', () => {
+  beforeAll(async () => {
+    const options = { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true };
+    // eslint-disable-next-line no-underscore-dangle
+    await mongoose.connect(global.__MONGO_URI__, options, (err) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+});
