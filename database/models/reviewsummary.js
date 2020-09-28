c839caa9c9d793dace481e43209505d33f0185cf
Tandy Nguyen <nguyetan001@gmail.com>
2020-09-28 02:52:29 -0400

    Created ReviewSummary model

---
+++ database/models/reviewsummary.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const reviewSummarySchema = new mongoose.Schema({
+  product_id: { type: Number, required: true, unique: true },
+  average_rating: Number,
+  stars_1: { type: Number, default: 0 },
+  stars_2: { type: Number, default: 0 },
+  stars_3: { type: Number, default: 0 },
+  stars_4: { type: Number, default: 0 },
+  stars_5: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model('ReviewSummary', reviewSummarySchema);
