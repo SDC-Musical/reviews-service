9e0a6cfa64031112f40fe9b5b66db36397d7e28c
Tandy Nguyen <nguyetan001@gmail.com>
2020-10-12 02:54:11 -0400

    Add middleware to handle CORS err for porxy server

--- server/app.js
+++ server/app.js
@@ -3,6 +3,11 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use('/', express.static(path.join(__dirname, '../public')));
 app.use('/api/reviews', require('./routes/reviews.js'));
 app.use('/api/reviewsummary', require('./routes/reviewsummary.js'));
