01ef5ccec0b36d09c3ee02bb03e2be61529499fb
Tandy Nguyen <nguyetan001@gmail.com>
2020-10-28 05:08:48 -0400

    Add sample data
    If API fails to get products 1 & 2, it will use sample data instead

--- client/hooks/useAPI.js
+++ client/hooks/useAPI.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import sampleReviews from '../sampleReviews.json';
+import sampleReviewSummary from '../sampleReviewSummary.json';
 
 const useAPI = (url) => {
   const [apiData, setApiData] = useState(null);
@@ -7,6 +9,10 @@ const useAPI = (url) => {
     fetch(url)
       .then((res) => {
         if (!res.ok) {
+          if (url.indexOf('reviews/1/summary') !== -1) return [sampleReviewSummary[0]];
+          if (url.indexOf('reviews/2/summary') !== -1) return [sampleReviewSummary[1]];
+          if (url.indexOf('reviews/1?limit=1') !== -1) return [sampleReviews[0]];
+          if (url.indexOf('reviews/2?limit=1') !== -1) return [sampleReviews[1]];
           throw new Error('Error fetching from API');
         }
         return res.json();
