02cc53443531de0b20bf508ddb82ea557ac35260
Nicholas Tolle <nbtolle@gmail.com>
2021-01-12 20:34:34 -0700

    Enable text compression

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const reviewRouter = require('./routes/reviews.js')
 const cors = require('cors');
+const expressStaticGzip = require('express-static-gzip');
 
 const app = express();
 
@@ -17,7 +18,14 @@ app.use((req, res, next) => {
 app.use(bodyParser.urlencoded());
 app.use('/api/reviews', reviewRouter);
 
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', expressStaticGzip(path.join(__dirname, '../public'), {
+  enableBrotli: true,
+  orderPreference: ['br', 'gz'],
+  setHeaders: (res, path) => {
+    res.setHeader('Cache-Control', 'public, max-age=31536000');
+  }
+}));
 app.use('/:id', express.static(path.join(__dirname, '../public')));
 // app.use('/api/reviews', require('./routes/reviews.js'));
 
